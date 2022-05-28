3b610d60ef72919c0fad87cbb5dacdabbffa3e73
Fityan <fityannugroho@gmail.com>
2022-05-28 19:46:56 +0700

    Install `npm run watch` requirements

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,18 @@ const mix = require('laravel-mix');
  |
  */
 
+// === Mix configuration for both production and development environments ===
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps();
+
+// === Mix configuration for development environment ===
+if (!mix.inProduction()) {
+    mix.disableNotifications();
+    mix.browserSync(process.env.MIX_BROWSER_SYNC_URL);
+}
+
+// === Mix configuration for production environment ===
+if (mix.inProduction()) {
+    mix.version();
+}
